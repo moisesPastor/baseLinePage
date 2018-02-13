5743b85b6f92087cb46dacdfbf215f0719ef26c5
moisesPastor <mpastorg@prhlt.upv.es>
2018-02-13 17:41:02 +0100

    working

--- SRC/Extremely_Randomized_Trees/ext_rand_trees.cc
+++ SRC/Extremely_Randomized_Trees/ext_rand_trees.cc
@@ -1,5 +1,21 @@
-//Compilar: g++ -o ext_rand_trees ext_rand_trees.cpp `pkg-config opencv --cflags --libs` 
-//g++ -o ext_rand_trees ext_rand_trees.cpp -I/usr/include/opencv -I/usr/include -L/usr/lib  -lopencv_highgui -lopencv_imgproc -lopencv_core -lopencv_ml -O3
+ *   Copyright 2017, Moisés Pastor i Gadea
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *
+ *  Created on: 20/10/2017
+ *      Author: Moisés Pastor i Gadea
+ */
+
 #include <iostream>
 #include <unistd.h>
 #include <fstream>
