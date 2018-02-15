85c058e26648257cc7ad6ab2b93e332e2c13e99f
moisesPastor <mpastorg@prhlt.upv.es>
2018-02-15 10:58:17 +0100

    working

--- SRC/Extremely_Randomized_Trees/ext_rand_trees.cc
+++ SRC/Extremely_Randomized_Trees/ext_rand_trees.cc
@@ -1,4 +1,4 @@
- *   Copyright 2017, Moisés Pastor i Gadea
+ /*   Copyright 2017, Moisés Pastor i Gadea
  *
  *   Licensed under the Apache License, Version 2.0 (the "License");
  *   you may not use this file except in compliance with the License.
