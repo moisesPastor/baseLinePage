688631573105d9e5410368951c4cfa4ecbe8f276
moisesPastor <mpastorg@prhlt.upv.es>
2018-02-21 10:27:05 +0100

    working

---
+++ SCRIPTS/extractLines.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ ! $# -eq 2 ]; then
+  echo "Use: ${0##*/} <testFileList> <Path_Dir_Output>"
+  exit 1
+fi
+
+#set -x : Display commands and their arguments as they are executed.
+#set -v : Display shell input lines as they are read.
+#set -n : Read commands but do not execute them. This may be used to check a shell script for syntax errors.
+#set -e stop the script if an error occurs
+
+PATH=$PATH:../../BIN:.
+DOUT=${2/\//}
+
+
+for file in `cat $1`; do
+     Nom=`basename $file .$EXT`
+     NOM=`echo -e ${DOUT}"/"${Nom}`
+     echo -e $NOM" "`lineExtractor -x ${NOM}.xml -i ${NOM}.jpg -t -v 1`
+done
