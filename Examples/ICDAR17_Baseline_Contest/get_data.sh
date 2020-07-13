dac3bbb637ab38509346937915dad75735a9ea1c
moisesPastor <mpastorg@prhtl.dsic.upv.es>
2020-07-13 12:54:48 +0200

    no descarrega els corpus, en canvi descarrega un script per baixar-los

---
+++ Examples/ICDAR17_Baseline_Contest/get_data.sh
@@ -0,0 +1,5 @@
+wget --no-check-certificate http://www.prhlt.upv.es/~mpastorg/ICDAR17_Baseline_Contest_data.tgz
+
+tar xvzf ICDAR17_Baseline_Contest_data.tgz
+rm ICDAR17_Baseline_Contest_data.tgz
+
---
+++ Examples/ICDAR17_READ_HTR_Contest_Corpus/get_data.sh
@@ -0,0 +1,5 @@
+wget --no-check-certificate http://www.prhlt.upv.es/~mpastorg/ICDAR17_READ_HTR_Contest_Corpus_data.tgz
+
+tar xvzf ICDAR17_READ_HTR_Contest_Corpus_data.tgz
+rm ICDAR17_READ_HTR_Contest_Corpus_data.tgz
+
